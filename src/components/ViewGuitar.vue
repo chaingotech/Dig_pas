de275e9884d8dae2f1ac36182ed76c12f6dfb198
Gonzalo Payo <gonzalo@bebup.es>
2021-11-30 17:28:47 +0100

    Added front and back images

--- src/components/ViewGuitar.vue
+++ src/components/ViewGuitar.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="d-flex">
-    <div class="element_front p-5 text-white w-50">
-      <img alt="" class=" " src="@/assets/front.svg" data-toggle="modal" data-target="#frontModal"
+    <div class="element-sides element_front p-5 text-white w-50">
+      <img alt="" class="" src="/images/sides/front.jpeg" width="116" data-toggle="modal" data-target="#frontModal"
            data-whatever="@getbootstrap">
       <div class="text-center pt-4 text-capitalize">{{ $t('front') }}</div>
     </div>
-    <div class="element_back p-5 text-white w-50" data-toggle="modal" data-target="#backModal"
+    <div class="element-sides element_back p-5 text-white w-50" data-toggle="modal" data-target="#backModal"
          data-whatever="@getbootstrap">
-      <img alt="" class=" " src="@/assets/back.svg"/>
+      <img alt="" class=" " src="/images/sides/back.jpeg" width="116" />
       <div class="text-center pt-4 text-capitalize">{{ $t('back') }}</div>
     </div>
 
@@ -18,7 +18,7 @@
           <div class="modal-body p-0 m-0">
             <h4 class="text-center my-4 text-uppercase">{{ $t('front') }}</h4>
 
-            <img alt="" class="img-dialog-guitar" src="@/assets/front.svg">
+            <img alt="" class="img-dialog-guitar" src="/images/sides/front.jpeg">
             <div class="close-dialog py-4 text-uppercase" data-dismiss="modal">{{ $t('close') }}</div>
           </div>
         </div>
@@ -32,7 +32,7 @@
         <div class="modal-content p-0 m-0">
           <div class="modal-body p-0 m-0">
             <h4 class="text-center my-4 text-uppercase">{{ $t('back') }}</h4>
-            <img alt="" class="img-dialog-guitar" src="@/assets/back.svg">
+            <img alt="" class="img-dialog-guitar" src="/images/sides/back.jpeg">
             <div class="close-dialog py-4" data-dismiss="modal">{{ $t('close') }}</div>
           </div>
         </div>
@@ -58,6 +58,12 @@ export default class ViewGuitar extends Vue {
 
 <style lang="scss">
 
+.element-sides {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
 .element_front {
   background: var(--main-bg-lighter-color);
 }
@@ -66,10 +72,13 @@ export default class ViewGuitar extends Vue {
   background: var(--main-bg-color);
 }
 
-.img-dialog-guitar {
-  height: 70vh;
-  margin: 0 auto;
-  width: 100%;
+.modal-body {
+  text-align: center;
+  .img-dialog-guitar {
+    margin: 0 auto;
+    max-height: 70vh;
+    max-width: 100%;
+  }
 }
 
 </style>
